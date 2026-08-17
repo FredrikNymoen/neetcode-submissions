27ca8a3a1d84659bb2e14d2fbba98ea59d1a82e9
Fredrik Nymoen <146934672+FredrikNymoen@users.noreply.github.com>
2026-08-18 01:24:42 +0200

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}
+
+        for i, num in enumerate(nums):
+            missing = target - num
+            if missing in prevMap:
+                return [prevMap[missing],i]
+            
+            prevMap[num] = i
+
+        return
