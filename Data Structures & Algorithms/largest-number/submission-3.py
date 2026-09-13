1a9eea15faceb3ade3978821aff91d5d16f33949
Fredrik Nymoen <146934672+FredrikNymoen@users.noreply.github.com>
2026-09-13 22:48:09 +0200

    Add: largest-number - submission-3

---
+++ Data Structures & Algorithms/largest-number/submission-3.py
@@ -0,0 +1,16 @@
+from functools import cmp_to_key
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        
+        arr = [str(num) for num in nums]
+
+        def compare(a,b):
+            if a + b > b + a:
+                return -1
+            else:
+                return 1
+        
+        arr.sort(key=cmp_to_key(compare))
+
+        return str(int("".join(arr)))
